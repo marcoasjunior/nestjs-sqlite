367a659f795fca86d0d84070198c5c8849567a0f
marcoasjunior <marco.silva@devoz.com.br>
2020-09-15 18:29:58 -0300

    skeleton

--- src/base/base.controller.ts
+++ src/base/base.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Post } from '@nestjs/common';
 @Controller('base')
-export class BaseController {}
+export class BaseController {
+
+  @Post()
+  create(): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
--- src/base/base.service.ts
+++ src/base/base.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Base } from './base.entity'
 
 @Injectable()
-export class BaseService {}
+export class BaseService {
+    
+  constructor(@InjectRepository(Base) private baseRepository: Repository<Base>) {}
+
+  findAll(): Promise<Base[]> {
+    return this.baseRepository.find()
+  }
+
+  findOne(id: string): Promise<Base> {
+    return this.baseRepository.findOne(id)
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.baseRepository.delete(id)
+  }
+}
+
